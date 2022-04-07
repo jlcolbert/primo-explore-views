6cf20c790b9a89a2c4d331b31d005ec3a2a72c78
Jay Colbert <jaylcolbert@gmail.com>
2022-04-07 18:28:19 -0400

    Removed unpaywall from main

--- custom/01USNH_UNH-MAIN/js/main.js
+++ custom/01USNH_UNH-MAIN/js/main.js
@@ -1,36 +1,28 @@
 import "@orbis-cascade/primo-explore-external-search";
 import "primo-explore-search-bar-sub-menu";
-import "primo-explore-unpaywall";
 
 import "./components/helpMenu/helpMenu.module";
 
 import ExternalSearchConfig from "./components/externalSearch/externalSearch.config";
 import HelpMenuConfig from "./components/helpMenu/helpMenu.config";
 import SearchBarSubMenuConfig from "./components/searchBarSubMenu/searchBarSubMenu.config";
-import UnpaywallConfig from "./components/unpaywall/unpaywall.config";
-import PRMSearchResultAvailabilityLineAfterDirective from "./directives/prmSearchResultAvailabilityLineAfter.directive";
 import PRMSearchBarAfterDirective from "./directives/prmSearchBarAfter.directive";
 
-// eslint-disable-next-line no-undef
 const app = angular.module("viewCustom", [
   "angularLoad",
   "externalSearch",
   "helpMenuContentDisplay",
   "helpMenuTopbar",
   "searchBarSubMenu",
-  "bulibUnpaywall",
 ]);
 
 // Adds the chat slide out
-// eslint-disable-next-line func-names
 (function () {
-  // eslint-disable-next-line no-undef
   const lc = document.createElement("script");
   lc.type = "text/javascript";
   lc.async = "true";
   lc.src =
     "https://v2.libanswers.com/load_chat.php?hash=17b1fa99b577483a418172851f40a779";
-  // eslint-disable-next-line no-undef
   const s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(lc, s);
 })();
@@ -38,10 +30,5 @@ const app = angular.module("viewCustom", [
 app
   .constant("helpMenuConfig", HelpMenuConfig)
   .constant("searchBarSubMenuItems", SearchBarSubMenuConfig)
-  .constant("unpaywallConfig", UnpaywallConfig)
-  .component(
-    "prmSearchResultAvailabilityLineAfter",
-    PRMSearchResultAvailabilityLineAfterDirective
-  )
   .component("prmSearchBarAfter", PRMSearchBarAfterDirective)
   .value("searchTargets", ExternalSearchConfig);
