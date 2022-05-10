b738828957d0ab7569167e0e699d105f87871f0b
Jay Colbert <jaylcolbert@gmail.com>
2022-05-10 14:20:19 -0400

    got rid of quicklink stuff

--- custom/01USNH_UNH-MAIN/js/components/quickLinks/quickLinks.component.js
+++
@@ -1,3 +0,0 @@
-const QuickLinksComponent = `<em>May not be published version</em>`;
-
-export default QuickLinksComponent;
--- custom/01USNH_UNH-MAIN/js/directives/prmQuickLinkAfter.directive.js
+++
@@ -1,8 +0,0 @@
-import QuickLinksComponent from "../components/quickLinks/quickLinks.component";
-
-const PRMQuickLinkAfterDirective = {
-  bindings: { parentCtrl: `<` },
-  template: QuickLinksComponent,
-};
-
-export default PRMQuickLinkAfterDirective;
