5bc1cca953abe8f53e2c74328f27e9ec96872d65
Jay Colbert <jaylcolbert@gmail.com>
2022-05-10 08:39:48 -0400

    Added permanent boxes for including facets

---
+++ custom/01USNH_UNH-MAIN/js/components/quickLinks/quickLinks.component.js
@@ -0,0 +1,3 @@
+const QuickLinksComponent = `<em>May not be published version</em>`;
+
+export default QuickLinksComponent;
---
+++ custom/01USNH_UNH-MAIN/js/directives/prmQuickLinkAfter.directive.js
@@ -0,0 +1,8 @@
+import QuickLinksComponent from "../components/quickLinks/quickLinks.component";
+
+const PRMQuickLinkAfterDirective = {
+  bindings: { parentCtrl: `<` },
+  template: QuickLinksComponent,
+};
+
+export default PRMQuickLinkAfterDirective;
